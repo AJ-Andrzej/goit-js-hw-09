283bcdf728a8cb48ff69255ab6b713d5f8b78da6
AJ-Andrzej <aayaschenko@gmail.com>
2024-03-31 20:42:12 +0200

    2-nd-task

---
+++ src/js/2-form.js
@@ -0,0 +1,33 @@
+const form = document.querySelector(".feedback-form")
+const storageKey = "feedback-form-state"
+const emailInput = form.elements.email;
+const messageInput = form.elements.message;
+
+form.addEventListener("input", formInputHandeler)
+form.addEventListener("submit", formSubmitHandler)
+
+function formInputHandeler(event) {
+    const email = emailInput.value;
+    const message = messageInput.value;
+    const data = JSON.stringify({ email, message });
+    localStorage.setItem(storageKey, data)
+}
+
+function formSubmitHandler(event) {
+    event.preventDefault();
+    const email = emailInput.value;
+    const message = messageInput.value;
+    console.log({ email, message })
+    localStorage.removeItem(storageKey)
+    form.reset();
+
+}
+const jsnData = localStorage.getItem(storageKey) ?? "";
+try {
+const inputData = JSON.parse(jsnData)
+emailInput.value = inputData.email;
+messageInput.value = inputData.message;
+} catch {
+emailInput.value = "";
+messageInput.value = "";
+}
